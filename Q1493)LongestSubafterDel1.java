6e4255e4db28359033e14a627b19f29090f6f7b3
Faisal Mujeeb <107312668+faisal-mujeeb@users.noreply.github.com>
2023-07-05 23:42:28 +0530

    Add files via upload

---
+++ Q1493)LongestSubafterDel1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+           
+            int n = nums.length;
+
+            int left = 0;
+            int zeros = 0;
+            int ans = 0;
+
+            for(int right =0; right<n; right++){
+                if(nums[right] == 0){
+                    zeros++;
+                }
+
+                while(zeros>1){
+                    if(nums[left] ==0){
+                        zeros--;
+                    }
+                    left++;
+                }
+                ans = Math.max(ans, rigth-left+1-zeros);
+            }
+            return (ans==n)?ans-1:ans;
+    }
+}
+
+//T.C----- O(N)
+//S.C------ O(1)
