0be37ee0726b22614de7161896d772e45b76bbea
Faisal Mujeeb <107312668+faisal-mujeeb@users.noreply.github.com>
2023-07-14 23:06:32 +0530

    Add files via upload

---
+++ LinkedListQues/PallindromOfLL.java
@@ -0,0 +1,50 @@
+public class PallindromOfLL {
+    class Solution {
+        public ListNode middleNode(ListNode head){
+            ListNode s = head;
+            ListNode f = head;
+            
+            while( f!= null && f.next!= null){
+                s = s.next;
+                f = f.next.next;
+            }
+            return s;
+        }
+        public ListNode reverseList(ListNode head){
+            if(head == null){
+                return head;
+            }
+            ListNode prev = null;
+            ListNode present = head;
+            ListNode next = present.next;
+            
+            while( present!= next){
+                present.next = prev;
+                prev = present;
+                present = next;
+                if(next!= null){
+                    next = next.next;
+                }
+            }
+            return prev;
+        }
+        public boolean isPalindrome(ListNode head) {
+            ListNode mid = middleNode(head);
+            ListNode headSecond = reverseList(mid);
+            ListNode reverseHead = headSecond;
+            
+            while (head != null && headSecond!=null){
+                if(head.val!= headSecond.val){
+                    break;
+                }
+                head = head.next;
+                headSecond = headSecond.next;
+            }
+            reverseList (reverseHead);
+            if (head == null || headSecond == null){
+                return true;
+            }
+            return false;
+        }
+    }
+}
